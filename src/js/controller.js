0b52cf411f7fd5771c0698b0f385d83443133dfc
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2023-12-19 12:24:29 +0200

    Loading a Recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5d');
+    // const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    /* console.log(response); */
+    const data = await response.json();
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    /* if (response.ok) console.log(data); */
+    let { recipe } = data.data;
+    /* console.log(recipe); */
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      ingredients: recipe.ingredients,
+      cookingTime: recipe.cooking_time
+    }
+    /* console.log(recipe); */
+  } catch (error) {
+    alert(error);
+  }
+}
+
+showRecipe();
