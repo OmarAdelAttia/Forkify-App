8830a4ad789ec24e924b84f64923e0d7248a1b7a
Omar Adel <69749119+Omar798-c@users.noreply.github.com>
2023-12-25 00:38:00 +0200

    parcel

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -585,28 +585,41 @@ const timeout = function(s) {
 };
 // https://forkify-api.herokuapp.com/v2
 ///////////////////////////////////////
-const showRecipe = async function() {
+// const showRecipe = async function () {
+//   try {
+//     const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5d');
+//     // const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+//     /* console.log(response); */
+//     const data = await response.json();
+//     if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+//     /* if (response.ok) console.log(data); */
+//     let { recipe } = data.data;
+//     /* console.log(recipe); */
+//     recipe = {
+//       id: recipe.id,
+//       title: recipe.title,
+//       image: recipe.image_url,
+//       servings: recipe.servings,
+//       publisher: recipe.publisher,
+//       sourceUrl: recipe.source_url,
+//       ingredients: recipe.ingredients,
+//       cookingTime: recipe.cooking_time
+//     }
+//     /* console.log(recipe); */
+//   } catch (error) {
+//     alert(error);
+//   }
+// }
+// showRecipe();
+const showCurrency = async function() {
     try {
-        const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5d");
-        // const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
-        /* console.log(response); */ const data = await response.json();
-        if (!response.ok) throw new Error(`${data.message} (${response.status})`);
-        /* if (response.ok) console.log(data); */ let { recipe } = data.data;
-        /* console.log(recipe); */ recipe = {
-            id: recipe.id,
-            title: recipe.title,
-            image: recipe.image_url,
-            servings: recipe.servings,
-            publisher: recipe.publisher,
-            sourceUrl: recipe.source_url,
-            ingredients: recipe.ingredients,
-            cookingTime: recipe.cooking_time
-        };
-    /* console.log(recipe); */ } catch (error) {
+        const response = await get("https://data.fixer.io/api/latest?access_key=334f4cd7e28c69e7e5be0698e50e9403");
+        console.log(response.data);
+    } catch (error) {
         alert(error);
     }
 };
-showRecipe();
+showCurrency();
 
 },{}]},["kYpTN","aenu9"], "aenu9", "parcelRequire3a11")
 
